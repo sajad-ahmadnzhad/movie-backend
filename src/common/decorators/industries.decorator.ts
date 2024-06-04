168aa735bc2433b2d1cf558027e843f1691ac0cf
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-06-04 15:04:21 +0430

    feat: add swagger for api create industry

--- src/common/decorators/industries.decorator.ts
+++ src/common/decorators/industries.decorator.ts
@@ -1,8 +1,22 @@
 import { UseGuards, applyDecorators } from "@nestjs/common";
 import { AuthGuard } from "../../modules/auth/guards/Auth.guard";
 import { IsAdminGuard } from "../../modules/auth/guards/isAdmin.guard";
+import {
+  ApiOperation,
+  ApiConflictResponse,
+  ApiOkResponse,
+  ApiInternalServerErrorResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+} from "@nestjs/swagger";
 
-//* Create industry decorator 
+//* Create industry decorator
 export const CreateIndustryDecorator = applyDecorators(
-    UseGuards(AuthGuard , IsAdminGuard)
-)
+  UseGuards(AuthGuard, IsAdminGuard),
+  ApiOperation({ summary: "create new industry" }),
+  ApiConflictResponse({ description: "Already exists industry" }),
+  ApiOkResponse({ description: "Created industry success" }),
+  ApiInternalServerErrorResponse({ description: "Jwt expired" }),
+  ApiForbiddenResponse({ description: "Forbidden resource" }),
+  ApiNotFoundResponse({ description: "Industry not found" })
+);
