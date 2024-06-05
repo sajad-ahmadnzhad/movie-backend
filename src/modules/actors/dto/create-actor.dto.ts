425e1d655de7b80712297b21010d8590d46e07bb
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-06-05 18:04:57 +0430

    feat: create dto actors

--- src/modules/actors/dto/create-actor.dto.ts
+++ src/modules/actors/dto/create-actor.dto.ts
@@ -1,1 +1,27 @@
-export class CreateActorDto {}
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+} from "class-validator";
+import { ObjectId } from "mongoose";
+import { PublicMessages } from "src/common/enum/public.messages";
+
+export class CreateActorDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 50)
+  name: string;
+  @IsString()
+  @Length(5, 100)
+  @IsOptional()
+  bio?: string;
+  @IsString()
+  @IsOptional()
+  avatar?: string;
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[0-9a-fA-F]{24}$/, { message: PublicMessages.InvalidObjectId })
+  country: ObjectId;
+}
