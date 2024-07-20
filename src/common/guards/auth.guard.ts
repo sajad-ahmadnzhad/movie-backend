5ff8eb6e933c438f2f764f086e81ba47144aa1a1
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-07-20 16:49:25 +0430

    feat: create role guard

---
+++ src/common/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { ROLES_KEY } from "../decorators/role.decorator";
+import { Roles } from "../enums/roles.enum";
+import { Request } from "express";
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    console.log(context.getHandler()());
+    const roles = this.reflector.get<Roles[]>(ROLES_KEY, context.getHandler());
+
+    if (!roles) return true;
+
+    const req = context.switchToHttp().getRequest() as Request;
+
+    const { user } = req;
+
+    return roles.some((role) => role == user?.role);
+  }
+}
