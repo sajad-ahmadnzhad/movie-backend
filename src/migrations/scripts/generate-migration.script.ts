de65642a01f8fef46ecc06b7baa57d5cc94660cf
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-07-29 13:11:05 +0430

    feat: create file script generate migration

---
+++ src/migrations/scripts/generate-migration.script.ts
@@ -0,0 +1,24 @@
+import { exec } from "child_process";
+
+const migrationName = process.argv[2];
+
+if (!migrationName) {
+  console.log("Please provide a migration name.");
+  process.exit(1);
+}
+
+const command = `npm run typeorm migration:generate ./src/migrations/${migrationName}`;
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+
+  if (stderr) {
+    console.error(`Error: ${stderr}`);
+    return;
+  }
+
+  console.log(`Stdout: ${stdout}`);
+});
